c3310ded077de33fd44f07be406a61878d3f4a34
FarinHeiT <yariksuharik85@gmail.com>
2019-09-14 13:12:36 +0300

    vue, reddit functionality, bootsrap

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request
 from parsers import redditParser
 
 app = Flask(__name__)
@@ -6,8 +6,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	data = jsonify(redditParser.get_reddit_json())
-	return data
+	return render_template('index.html')
+
+
+@app.route('/reddit.json')
+def reddit_json():
+	subreddit = request.args.get('subreddit')
+	sort = request.args.get('sort')
+
+	return jsonify(redditParser.get_reddit_json(subreddit, sort))
+
 
 
 if __name__ == '__main__':
